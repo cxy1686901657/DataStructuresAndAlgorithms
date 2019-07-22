ae870c9209ccce73f9b4d4b8de0750ce7cfe1601
cxy1686901657 <1686901657@qq.com>
2019-07-22 11:21:50 +0800

    递归实现 迷宫回溯问题

---
+++ DataStructures/src/com/qc/recursion/MiGong.java
@@ -0,0 +1,86 @@
+package com.qc.recursion;
+
+/**
+ * @author qc
+ * @date 2019/7/21
+ */
+
+public class MiGong {
+    public static void main(String[] args) {
+        // 先创建一个二维数组，模拟迷宫
+        // 地图
+        int[][] map = new int[8][7];
+        // 使用1 表示墙
+        // 上下全部置为1
+        for (int i = 0; i < 7; i++) {
+            map[0][i] = 1;
+            map[7][i] = 1;
+        }
+
+        // 左右全部置为1
+        for (int i = 0; i < 8; i++) {
+            map[i][0] = 1;
+            map[i][6] = 1;
+        }
+        //设置挡板, 1 表示
+        map[3][1] = 1;
+        map[3][2] = 1;
+//		map[1][2] = 1;
+//		map[2][2] = 1;
+
+        // 输出地图
+        System.out.println("地图的情况");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        setWay(map, 1, 1);
+
+        // 输出地图
+        System.out.println("地图的情况");
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+    /**
+     *
+     * @param map 表示地图
+     * @param i 从哪个位置开始找
+     * @param j
+     * @return 如果找到通路，就返回true, 否则返回false
+     */
+    public static boolean setWay(int[][] map, int i, int j) {
+        if(map[6][5] == 2) { // 通路已经找到ok
+            return true;
+        } else {
+            if(map[i][j] == 0) { //如果当前这个点还没有走过
+                //按照策略 下->右->上->左  走
+                map[i][j] = 2; // 假定该点是可以走通.
+                if(setWay(map, i+1, j)) {//向下走
+                    return true;
+                } else if (setWay(map, i, j+1)) { //向右走
+                    return true;
+                } else if (setWay(map, i-1, j)) { //向上
+                    return true;
+                } else if (setWay(map, i, j-1)){ // 向左走
+                    return true;
+                } else {
+                    //说明该点是走不通，是死路
+                    map[i][j] = 3;
+                    return false;
+                }
+            } else { // 如果map[i][j] != 0 , 可能是 1， 2， 3
+                return false;
+            }
+        }
+    }
+}
