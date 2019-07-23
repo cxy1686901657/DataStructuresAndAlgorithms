d14730fe07e1f165bad5e54cae7534743bfc1dda
cxy1686901657 <1686901657@qq.com>
2019-07-23 16:42:08 +0800

    选择排序

--- DataStructures/src/com/qc/sort/SelectSort.java
+++ DataStructures/src/com/qc/sort/SelectSort.java
@@ -1,5 +1,8 @@
 package com.qc.sort;
 
+import java.time.Duration;
+import java.time.Instant;
+
 /**
  * @author qc
  * @date 2019/7/22
@@ -7,15 +10,26 @@ package com.qc.sort;
 
 public class SelectSort {
     public static void main(String[] args) {
-        int[] arr = {101, 34, 119, 1};
-        selectSort(arr);
-        for (int a : arr) {
-            System.out.printf("%d\t\t", a);
+//        int[] arr = {101, 34, 119, 1};
+//        selectSort(arr);
+//        for (int a : arr) {
+//            System.out.printf("%d\t\t", a);
+//        }
+
+
+        Instant pre=Instant.now();
+        int[] arr2 = new int[80000];
+        for(int i =0; i < 80000;i++) {
+            arr2[i] = (int)(Math.random() * 8000000); //生成一个[0, 8000000) 数
         }
+        selectSort(arr2);
+        Instant now=Instant.now();
+        System.out.println(Duration.between(pre, now));
     }
 
     //选择排序时间复杂度是 O(n^2)
     public static void selectSort(int[] arr) {
+        int count=0;
         for (int i = 0; i < arr.length - 1; i++) {
             int minIndex = i;
             int min = arr[i];
@@ -23,6 +37,7 @@ public class SelectSort {
                 if (min > arr[j]) {
                     min = arr[j];
                     minIndex = j;
+                    count++;
                 }
             }
             // 将最小值，放在arr[0], 即交换
@@ -31,5 +46,6 @@ public class SelectSort {
                 arr[i] = min;
             }
         }
+        System.out.println(count);
     }
 }
