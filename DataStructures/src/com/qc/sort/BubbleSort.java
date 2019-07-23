1e6e568c9609c0688509033fd8cae551bec27c9e
cxy1686901657 <1686901657@qq.com>
2019-07-23 16:40:23 +0800

    冒泡排序

--- DataStructures/src/com/qc/sort/BubbleSort.java
+++ DataStructures/src/com/qc/sort/BubbleSort.java
@@ -29,6 +29,7 @@ public class BubbleSort {
         // 冒泡排序 的时间复杂度 O(n^2), 自己写出
         int temp = 0; // 临时变量
         boolean flag = false; // 标识变量，表示是否进行过交换
+        int count=0;
         for (int i = 0; i < arr.length - 1; i++) {
 
             for (int j = 0; j < arr.length - 1 - i; j++) {
@@ -38,6 +39,7 @@ public class BubbleSort {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    count++;
                 }
             }
             if (!flag) { // 在一趟排序中，一次交换都没有发生过
@@ -46,6 +48,7 @@ public class BubbleSort {
                 flag = false; // 重置flag!!!, 进行下次判断
             }
         }
+        System.out.println(count);
     }
     public static void print(int[] array){
         for (int a :array){
